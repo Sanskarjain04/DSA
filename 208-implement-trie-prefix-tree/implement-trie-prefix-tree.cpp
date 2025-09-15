34736bdec645ed378a2d984dcd9fcd6f459ec60c
Sanskarjain04 <116282600+Sanskarjain04@users.noreply.github.com>
2025-09-15 19:50:46 +0530

    Time: 71 ms (8.16%) | Memory: 137.6 MB (5.27%) - LeetSync

--- 208-implement-trie-prefix-tree/implement-trie-prefix-tree.cpp
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.cpp
@@ -1,9 +1,11 @@
 class TrieNode {
 public:
+    char data;
     TrieNode* children[26];
     bool isTerminal;
 
     TrieNode(char ch) {
+        data = ch;
         for (int i = 0; i < 26; i++) {
             children[i] = NULL;
         }
