b3365c8d0c4ffdd900b95e6240a08d2042b59e11
Sanskarjain04 <116282600+Sanskarjain04@users.noreply.github.com>
2025-09-20 16:54:22 +0530

    Time: 0 ms (100.00%) | Memory: 36.8 MB (54.14%) - LeetSync

--- 4012-earliest-time-to-finish-one-task/earliest-time-to-finish-one-task.cpp
+++ 4012-earliest-time-to-finish-one-task/earliest-time-to-finish-one-task.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int earliestTime(vector<vector<int>>& tasks) {
         int shortestTime = 1e9;
-        for(auto arr:tasks){
+        for(auto &arr:tasks){
             shortestTime = min(shortestTime,(arr[0]+arr[1]));
         }
         return shortestTime;
